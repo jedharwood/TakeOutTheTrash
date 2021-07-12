abced901062fb7378a54b70da08e3517bbe0f8b8
Jed Harwood <scaryteeth@hotmail.com>
2021-07-12 09:11:29 +0900

    Configured Store

--- takeoutthetrash-ui/src/js/Api/Fetch.js
+++ takeoutthetrash-ui/src/js/Api/Fetch.js
@@ -1,4 +1,20 @@
-import { handleResponse, handleError } from "./apiUtils";
+//import { handleResponse, handleError } from "./ApiUtils";
+
+const handleResponse = (response) => {
+  if (response.ok) return response.json();
+  if (response.status === 400) {
+    const error = response.text();
+    throw new Error(error);
+  }
+  throw new Error("Network response was not ok.");
+};
+
+// Log/handle error
+const handleError = (error) => {
+  // eslint-disable-next-line no-console
+  console.error("API call failed. " + error);
+  throw error;
+};
 
 export function get(url) {
   return fetch(url).then(handleResponse).catch(handleError);
---
+++ takeoutthetrash-ui/src/js/Reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+import cities from "./Cities/index";
+
+export default combineReducers({
+  cities,
+});
