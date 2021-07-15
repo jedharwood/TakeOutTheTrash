bd136e3b21c239f6b753518b2f78e5048d5b6408
Jed Harwood <scaryteeth@hotmail.com>
2021-07-15 10:20:34 +0900

    Rendering prefectures dropdown and dispatching prefectureId onChange

--- takeoutthetrash-ui/src/js/Components/SelectCityForm.js
+++ takeoutthetrash-ui/src/js/Components/SelectCityForm.js
@@ -3,24 +3,48 @@ import PropTypes from "prop-types";
 import * as prefecturesSelectors from "../Selectors/Prefectures/index";
 import * as prefecturesActions from "../Actions/Prefectures/index";
 import { connect } from "react-redux";
+//import * as R from "ramda";
+import { mapOptions } from "../Utilities/RamdaUtilities";
+
+// const mapOptions = R.map((item) => (
+//   <option key={item.id} value={item.id}>
+//     {item.name}
+//   </option>
+// ));
 
 const SelectPrefecture = ({ prefecturesList, selectPrefecture }) => {
-  return <select />;
+  return (
+    <div>
+      <select
+        id="prefecture_select_input"
+        onChange={(e) =>
+          selectPrefecture(e.target.value, e.target.selectedOptions[0].text)
+        }
+      >
+        <option value="">Please select...</option>
+        {mapOptions(prefecturesList)}
+      </select>
+    </div>
+  );
 };
 
-const SelectCityForm = () => {
-  return <SelectPrefecture />;
+const SelectCityForm = ({ prefecturesList, selectPrefecture }) => {
+  return (
+    <SelectPrefecture
+      prefecturesList={prefecturesList}
+      selectPrefecture={selectPrefecture}
+    />
+  );
 };
 
 SelectCityForm.propTypes = {
-  //   prefecturesList: PropTypes.arrayOf(
-  //     PropTypes.shape({
-  //       id: PropTypes.number.isRequired,
-  //       name: PropTypes.string.isRequired,
-  //       hasCities: PropTypes.bool.isRequired,
-  //     })
-  //   ),
-  prefecturesList: PropTypes.arrayOf(PropTypes.object),
+  prefecturesList: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      hasCities: PropTypes.bool.isRequired,
+    })
+  ),
   selectPrefecture: PropTypes.func,
   selectedPrefectureId: PropTypes.number,
 };
