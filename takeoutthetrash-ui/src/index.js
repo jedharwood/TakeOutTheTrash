8ca736d24a4b9c70b8fe4f366f54b0eac282b0df
Jed Harwood <scaryteeth@hotmail.com>
2021-12-17 22:32:07 +0900

    Added Sass
    Imported Sass, replaced app.css, aded footer, basic styling for header and footer.

--- takeoutthetrash-ui/src/index.js
+++ takeoutthetrash-ui/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./js/App";
 import store from "./js/configureStore";
 import { fetchPrefecturesList } from "./js/Actions/Prefectures/index";
--- takeoutthetrash-ui/src/js/Components/Common/Header.js
+++ takeoutthetrash-ui/src/js/Components/Common/Header.js
@@ -3,11 +3,30 @@ import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
-    <nav>
-      <NavLink exact to="/">
-        Home
-      </NavLink>
-    </nav>
+    <div id="header">
+      <div className="title">
+        <h1>Mottainai</h1>
+      </div>
+      <nav>
+        <NavLink
+          exact
+          to="/"
+          className="nav-link"
+          activeClassName="active-nav-link"
+        >
+          Home
+        </NavLink>
+        {" | "}
+        <NavLink
+          exact
+          to="/feedback"
+          className="nav-link"
+          activeClassName="active-nav-link"
+        >
+          Feedback
+        </NavLink>
+      </nav>
+    </div>
   );
 }
 
