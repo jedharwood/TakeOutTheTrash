0a2dbbf0d939b2c1019603d5893c49bb3d1ebb7b
Jed Harwood <scaryteeth@hotmail.com>
2022-02-01 16:33:00 +0900

    Feedback action tests
    tests for the feedback redux actions

---
+++ takeoutthetrash-ui/src/js/Actions/Feedback/__tests__/index.test.js
@@ -0,0 +1,135 @@
+import * as sut from "../index";
+import * as fetch from "../../../Api/Fetch";
+import * as resourceUtilities from "../../../Utilities/ResourceUtilities";
+import * as actionTypes from "../../../Constants/ActionType";
+import * as citiesSelectors from "../../../Selectors/Cities/index";
+import * as testData from "../../../CommonTestData/TestData";
+
+describe("feedback action", () => {
+  describe("postFeedbackForm", () => {
+    test("when result is not ok should dispatch a POST_FEEDBACK_FORM_FAILED action", async () => {
+      // Arrange
+      const feedbackFormApiUrl = "feedbackFormApiUrl";
+
+      citiesSelectors.getCity = jest
+        .fn()
+        .mockReturnValue(testData.cityWithRules);
+
+      resourceUtilities.getFeedbackFormApiUrl = jest
+        .fn()
+        .mockReturnValue(feedbackFormApiUrl);
+
+      const response = {
+        ok: false,
+      };
+
+      fetch.postJson = jest.fn(
+        () => new Promise((resolve) => resolve(response))
+      );
+
+      const dispatch = jest.fn();
+      const getState = jest.fn();
+
+      // Act
+      await sut.postFeedbackForm()(dispatch, getState);
+
+      // Assert
+      expect(dispatch.mock.calls.length).toEqual(2);
+
+      expect(dispatch.mock.calls[0]).toEqual([
+        {
+          type: actionTypes.POST_FEEDBACK_FORM_REQUESTED,
+        },
+      ]);
+
+      expect(dispatch.mock.calls[1]).toEqual([
+        {
+          type: actionTypes.POST_FEEDBACK_FORM_FAILED,
+        },
+      ]);
+    });
+
+    test("when fetch throws an error should dispatch a POST_FEEDBACK_FORM_FAILED action", async () => {
+      // Arrange
+      const feedbackFormApiUrl = "feedbackFormApiUrl";
+
+      citiesSelectors.getCity = jest
+        .fn()
+        .mockReturnValue(testData.cityWithRules);
+
+      resourceUtilities.getFeedbackFormApiUrl = jest
+        .fn()
+        .mockReturnValue(feedbackFormApiUrl);
+
+      fetch.postJson = jest.fn(
+        () =>
+          new Promise(() => {
+            throw new Error("bang");
+          })
+      );
+
+      const dispatch = jest.fn();
+      const getState = jest.fn();
+
+      // Act
+      await sut.postFeedbackForm()(dispatch, getState);
+
+      // Assert
+      expect(dispatch.mock.calls.length).toEqual(2);
+
+      expect(dispatch.mock.calls[0]).toEqual([
+        {
+          type: actionTypes.POST_FEEDBACK_FORM_REQUESTED,
+        },
+      ]);
+
+      expect(dispatch.mock.calls[1]).toEqual([
+        {
+          type: actionTypes.POST_FEEDBACK_FORM_FAILED,
+        },
+      ]);
+    });
+
+    test("when result is ok should dispatch a POST_FEEDBACK_FORM_SUCCEEDED action", async () => {
+      // Arrange
+      const feedbackFormApiUrl = "feedbackFormApiUrl";
+
+      citiesSelectors.getCity = jest
+        .fn()
+        .mockReturnValue(testData.cityWithRules);
+
+      resourceUtilities.getFeedbackFormApiUrl = jest
+        .fn()
+        .mockReturnValue(feedbackFormApiUrl);
+
+      const response = {
+        ok: true,
+      };
+
+      fetch.postJson = jest.fn(
+        () => new Promise((resolve) => resolve(response))
+      );
+
+      const dispatch = jest.fn();
+      const getState = jest.fn();
+
+      // Act
+      await sut.postFeedbackForm()(dispatch, getState);
+
+      // Assert
+      expect(dispatch.mock.calls.length).toEqual(2);
+
+      expect(dispatch.mock.calls[0]).toEqual([
+        {
+          type: actionTypes.POST_FEEDBACK_FORM_REQUESTED,
+        },
+      ]);
+
+      expect(dispatch.mock.calls[1]).toEqual([
+        {
+          type: actionTypes.POST_FEEDBACK_FORM_SUCCEEDED,
+        },
+      ]);
+    });
+  });
+});
