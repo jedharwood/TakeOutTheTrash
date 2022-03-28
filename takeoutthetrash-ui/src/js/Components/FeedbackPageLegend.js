f6f76a194e1a991a78a131fecf12cc7703b355d8
Jed Harwood <scaryteeth@hotmail.com>
2022-03-28 16:26:33 +0900

    General refactoring
    replace arrayIsEmpty method with ramda.isEmpty, removed some redundant areas of code, general cleanup

--- takeoutthetrash-ui/src/js/Components/FeedbackPageLegend.js
+++ takeoutthetrash-ui/src/js/Components/FeedbackPageLegend.js
@@ -13,7 +13,7 @@ export const getFeedbackPageLegend = (
   if (displayRetryFailureMessage === true) {
     return "Sorry, it appears something went wrong. We will work to resolve the problem as quickly as possible.";
   }
-  return "Thankyou for submitting your feedback. If you checked the radio button on the form (that I haven't made yet) to request email notification then we will be in contact to let you know when your request has been actioned.";
+  return "Thankyou for submitting your feedback. If you checked the radio button on the form to request email notification then we will be in contact to let you know when your request has been actioned.";
 };
 
 const FeedbackPageLegend = ({
--- takeoutthetrash-ui/src/js/Components/RulesDisplay.js
+++ takeoutthetrash-ui/src/js/Components/RulesDisplay.js
@@ -1,30 +1,19 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
-import * as citiesSelectors from "../Selectors/Cities/index";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import * as R from "ramda";
 import RulesModal from "./RulesModal";
-import * as rulesModalActions from "../Actions/RulesModal/index";
 import * as feedbackActions from "../Actions/Feedback/index";
+import * as rulesModalActions from "../Actions/RulesModal/index";
+import * as citiesSelectors from "../Selectors/Cities/index";
 
 const RulesDisplay = ({
   fetchingCitySucceeded,
-  city,
   openRulesModal,
   openFeedbackFormButtonClicked,
 }) => {
   if (!fetchingCitySucceeded) {
     return null;
-  }
-
-  if (R.isEmpty(city.rules)) {
-    return (
-      <div>
-        Sorry - looks like we've not added the rules for your city, yet. Maybe
-        try moving to Yokohama, I'm pretty sure they have rules there...
-      </div>
-    );
   }
 
   return (
@@ -59,42 +48,18 @@ const RulesDisplay = ({
 };
 
 RulesDisplay.propTypes = {
+  fetchingCitySucceeded: PropTypes.bool.isRequired,
   openRulesModal: PropTypes.func.isRequired,
   openFeedbackFormButtonClicked: PropTypes.func.isRequired,
-  fetchingCitySucceeded: PropTypes.bool.isRequired,
-  city: PropTypes.shape({
-    id: PropTypes.number,
-    name: PropTypes.string,
-    prefecture: PropTypes.shape({
-      id: PropTypes.number,
-      name: PropTypes.string,
-    }),
-    rating: PropTypes.number,
-    rules: PropTypes.arrayOf(
-      PropTypes.shape({
-        id: PropTypes.number,
-        name: PropTypes.string,
-        types: PropTypes.arrayOf(
-          PropTypes.shape({
-            name: PropTypes.string,
-            description: PropTypes.string,
-            instructions: PropTypes.string,
-            irregularFrequency: PropTypes.string,
-          })
-        ),
-      })
-    ),
-  }),
 };
 
 const mapStateToProps = (state) => ({
   fetchingCitySucceeded: citiesSelectors.fetchingCitySucceeded(state),
-  city: citiesSelectors.getCity(state),
 });
 
 const mapDispatchToProps = {
-  openRulesModal: rulesModalActions.openRulesModalButtonClicked,
   openFeedbackFormButtonClicked: feedbackActions.openFeedbackFormButtonClicked,
+  openRulesModal: rulesModalActions.openRulesModalButtonClicked,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(RulesDisplay);
