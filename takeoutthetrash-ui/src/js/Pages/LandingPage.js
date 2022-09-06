760da9a9e2cdadabd81cf224ee9d365f3feaf868
Jed Harwood <scaryteeth@hotmail.com>
2022-09-07 08:53:53 +0900

    Restyled landing page
    with tailwind css

--- takeoutthetrash-ui/src/js/Pages/LandingPage.js
+++ takeoutthetrash-ui/src/js/Pages/LandingPage.js
@@ -8,17 +8,21 @@ import * as citiesSelectors from "../Selectors/Cities";
 
 const LandingPage = ({ isFetchingCities, isFetchingCity }) => {
   return (
-    <div className="container">
-      <div className="landing-page">
-        <p>
-          Do you know if that bottle goes out on PET bottle day or plastic day?
-        </p>
-        <p>Do you know which bag to put your aluminium cans in?</p>
-        <h2>Do you know where you live???</h2>
-        <SelectCityForm errors={{ city: "", prefecture: "" }} />
-        <RulesDisplay />
-        <FetchingStateSpinner isVisible={isFetchingCities || isFetchingCity} />
+    <div>
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold landing-page">Do you know...</h2>
+            <p className="mt-2 text-center landing-page">...if that bottle goes out on PET bottle day or plastic day?</p>
+            <p className="text-center landing-page">Do you know which bag to put your aluminium cans in?</p>
+            <h2 className="mt-3 text-center text-xl font-extrabold landing-page">Do you even know where you live???</h2>
+          </div>
+          <SelectCityForm errors={{ city: "", prefecture: "" }} />
+        </div>
       </div>
+
+      <RulesDisplay />
+      <FetchingStateSpinner isVisible={isFetchingCities || isFetchingCity} />
     </div>
   );
 };
