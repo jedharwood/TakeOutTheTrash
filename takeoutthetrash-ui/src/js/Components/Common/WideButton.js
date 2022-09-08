81bbb5410fe6c37faa8fb508ab9d93aaf2951d97
Jed Harwood <scaryteeth@hotmail.com>
2022-09-08 11:59:02 +0900

    Made radio button tabbable
    RefactoredWideButton

--- takeoutthetrash-ui/src/js/Components/Common/WideButton.js
+++ takeoutthetrash-ui/src/js/Components/Common/WideButton.js
@@ -7,17 +7,21 @@ export const getButtonType = (type) => {
   return !isNilOrEmpty(type) ? type : "button";
 };
 
+const handleOnClick = (onClick) => {
+  return () => onClick();
+};
+
 export const WideButton = ({ type, route, onClick, disabled, buttonText }) => {
   const classes = "relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 button-wide";
 
   return route ? (
     <Link to={route}>
-      <button type={getButtonType(type)} onClick={onClick} className={`${classes} mt-6`} disabled={disabled}>
+      <button type={getButtonType(type)} onClick={handleOnClick(onClick)} className={`${classes} mt-6`} disabled={disabled}>
         {buttonText}
       </button>
     </Link>
   ) : (
-    <button type={getButtonType(type)} onClick={onClick} className={classes} disabled={disabled}>
+    <button type={getButtonType(type)} onClick={handleOnClick(onClick)} className={classes} disabled={disabled}>
       {buttonText}
     </button>
   );
